daebb1431dd5220c31105f29b38d16f32474baa4
Jhonattas Henrique <jhonattas@soucriador.com>
2017-10-12 10:30:22 -0300

    envia a primeira chamada do metodo do vhsys_api para o servidor

--- src/server.js
+++ src/server.js
@@ -5,9 +5,10 @@ vhsys_url = 'https://www.vhsys.com/Communication/WebService/'
 var options = { method: 'POST',
   url: vhsys_url,
   headers: 
-   { 'postman-token': '19da0b34-988b-d50d-829e-32f64f89375c',
+   {
      'cache-control': 'no-cache',
-     'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
+     'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+    },
   formData: 
    { TOKEN: 'AAGIjM2UDN0IbNUDbNXTDMyADc0BbN0Y7M',
      API: 'Pedidos',
